1f0b4e900bb03631ea87ed3f9ba699cfeb9424c8
MrNele <nemanja1225@gmail.com>
2021-12-12 20:37:58 +0100

    Add files via upload
    - set up XCode project
    - added Cocoapods to the project

---
+++ YoutubeApp/YoutubeApp/YoutubeAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  YoutubeAppApp.swift
+//  YoutubeApp
+//
+//  Created by iMac on 11.12.21..
+//
+
+import SwiftUI
+
+@main
+struct YoutubeAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
